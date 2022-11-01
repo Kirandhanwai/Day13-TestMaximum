eaef0052379cc75709812a42e2f42862a1099553
Kirandhanwai <kirandhanwai123@gmail.com>
2022-11-02 01:58:58 +0530

    Added: Given Three String FInd the Maximum

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to check max of three variables");
-		Float x = 1.2f, y = 3.4f, z = 0.45f;
+		String x = "jack", y = "king", z = "queen";
 		TestMaximum.testMaximum(x, y, z);
 	}
-
 }
--- src/com/bridgelabz/TestMaximum.java
+++ src/com/bridgelabz/TestMaximum.java
@@ -1,18 +1,18 @@
 package com.bridgelabz;
 
 public class TestMaximum {
-	int x, y, z;
+	String x, y, z;
 
 //constructor
-	public TestMaximum(int x, int y, int z) {
+	public TestMaximum(String x, String y, String z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 
 //checking max of three integers
-	public static Float testMaximum(Float x, Float y, Float z) {
-		Float max = x;
+	public static String testMaximum(String x, String y, String z) {
+		String max = x;
 		if (y.compareTo(max) > 0)
 			max = y;
 
@@ -23,7 +23,7 @@ public class TestMaximum {
 
 	}
 
-	public static void printMax(Float x, Float y, Float z, Float max) {
-		System.out.printf("\nThe max of %s , %s and %s \n is %s\n", x, y, z, max);
+	public static void printMax(String x, String y, String z, String max) {
+		System.out.printf("\nThe max of %s , %s and %s\n is %s\n", x, y, z, max);
 	}
 }
