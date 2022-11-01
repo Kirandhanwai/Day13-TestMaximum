f171968afad06ceb47994eb0242972ec0fee3964
Kirandhanwai <kirandhanwai123@gmail.com>
2022-11-02 01:36:40 +0530

    Added: Given Three Floats Find the Maximum

--- src/com/bridgelabz/TestMaximum.java
+++ src/com/bridgelabz/TestMaximum.java
@@ -1,32 +1,29 @@
 package com.bridgelabz;
 
 public class TestMaximum {
-
 	int x, y, z;
 
-	// constructor
+//constructor
 	public TestMaximum(int x, int y, int z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 
-	// checking max of three integers
-	public static Integer testMaximum(Integer x, Integer y, Integer z) {
-
-		Integer max = x;
+//checking max of three integers
+	public static Float testMaximum(Float x, Float y, Float z) {
+		Float max = x;
 		if (y.compareTo(max) > 0)
 			max = y;
 
 		if (z.compareTo(max) > 0)
 			max = z;
-
 		printMax(x, y, z, max);
 		return max;
 
 	}
 
-	public static void printMax(Integer x, Integer y, Integer z, Integer max) {
-		System.out.printf("\nThe max variable of %s, %s and %s \n is %s\n", x, y, z, max);
+	public static void printMax(Float x, Float y, Float z, Float max) {
+		System.out.printf("\nThe max of %s , %s and %s \n is %s\n", x, y, z, max);
 	}
 }
